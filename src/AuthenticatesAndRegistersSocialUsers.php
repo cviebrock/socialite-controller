61fafc221fb8348c39afa1f8b58cf43723f6b4b7
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-01-30 13:52:30 -0600

    handle scopes

--- src/AuthenticatesAndRegistersSocialUsers.php
+++ src/AuthenticatesAndRegistersSocialUsers.php
@@ -2,17 +2,40 @@
 
 use Socialize;
 
+
 trait AuthenticatesAndRegistersSocialUsers {
+
+	/**
+	 * The Guard implementation.
+	 *
+	 * @var Guard
+	 */
+	protected $auth;
+
+	/**
+	 * The registrar implementation.
+	 *
+	 * @var Registrar
+	 */
+	protected $registrar;
+
+	/**
+	 * @var Socialite
+	 */
+	protected $socialite;
 
 	public function getLogin($provider) {
 
-		return Socialize::with($provider)->redirect();
+		if ($scopes = config('services.' . $provider . '.scopes', [])) {
+			return $this->socialite->driver($provider)->scopes($scopes)->redirect();
+		}
+
+		return $this->socialite->driver($provider)->redirect();
 	}
 
 	public function getCallback($provider) {
-		$user = Socialize::with($provider)->user();
+		$user = $this->socialite->driver($provider)->user();
 
 		dd($user);
 	}
-
 }
